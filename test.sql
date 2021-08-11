dff31ffaaa5308fb7680e0fb27c09e6cefbfdeff
Gigel <cristian.lungeanu@stud.acs.upb.ro>
2021-08-11 18:58:25 +0300

    test tables

---
+++ test.sql
@@ -0,0 +1,10 @@
+﻿DELETE FROM dbo.MortiiMatii;
+DBCC CHECKIDENT ('MortiiMatii', RESEED, 0)
+
+insert into dbo.MortiiMatii (Username, Password) values ('1','1');
+insert into dbo.MortiiMatii (Username, Password) values ('2','2');
+insert into dbo.MortiiMatii (Username, Password) values ('3','3');
+insert into dbo.MortiiMatii (Username, Password) values ('4','4');
+insert into dbo.MortiiMatii (Username, Password) values ('5','5');
+
+select * from dbo.MortiiMatii;
